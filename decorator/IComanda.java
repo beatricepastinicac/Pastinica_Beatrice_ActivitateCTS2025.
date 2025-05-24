98b190783f785e686a3d99d3885915bb1f7023be
beatricepastinicac <pastinicacostinela22@stud.ase.ro>
2025-05-24 11:26:49 +0300

    Delete decorator directory

--- decorator/IComanda.java
+++
@@ -1,5 +0,0 @@
-package subiecte.d.s7.decorator;
-
-public interface IComanda {
-    void plaseazaComanda(Client client, double valoareCos, String adresa);
-}
--- decorator/IProdus.java
+++
@@ -1,6 +0,0 @@
-package subiecte.d.s4.decorator;
-
-public interface IProdus {
-    String getDescriere();
-    double getPret();
-}
