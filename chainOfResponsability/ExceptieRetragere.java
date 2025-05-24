99738287fd101f7aee132f9182a94f8ae696b652
beatricepastinicac <pastinicacostinela22@stud.ase.ro>
2025-05-24 11:26:16 +0300

    Delete chainOfResponsability directory

--- chainOfResponsability/ExceptieRetragere.java
+++
@@ -1,7 +0,0 @@
-package subiecte.d.s11.chainOfResponsability;
-
-public class ExceptieRetragere extends Exception {
-    public ExceptieRetragere(String msg) {
-        super(msg);
-    }
-}
--- chainOfResponsability/ICautare.java
+++
@@ -1,7 +0,0 @@
-package subiecte.d.s10.chainOfResponsability;
-
-import java.util.List;
-
-public interface ICautare {
-    List<Produs> cauta(List<Produs> produse);
-}
